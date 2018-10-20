4fe25da3c0e81da757ab5843f5f9f15bc54d8372
etlonjonh <43503399+etlonjonh@users.noreply.github.com>
2018-10-20 13:37:06 -0500

    P'S ADDED

--- areas/SectionP.js
+++ areas/SectionP.js
@@ -22,12 +22,77 @@ var SectionN = require('./areas/SectionO');
 var SectionO = require('./areas/SectionP');
 
 module.exports = {
-    foo: function () {
-        // whatever
-        console.log("Hello World!!! from FOO")
-    },
-    bar: function () {
-        // whatever
-        console.log("Hello World!!! from BAR")
+    MapP5: function () {
+        console.log("P5");
+        let directions = ['North', 'West', 'East', 'South'],
+            index = readlineSync.keyInSelect(directions, 'What do you do now??');
+
+        if (directions[index] == "North") {
+            console.log("Cant Move Here!!!");
+            MapP5();
+        }
+
+        if (directions[index] == "West") {
+            console.log("MOVED WEST!!");
+            MapO5();
+        }
+
+        if (directions[index] == "East") {
+            console.log("CANT MOVE HERE!!");
+            MapP5();
+        }
+
+        if (directions[index] == "South") {
+            console.log("YOU MOVED SOUTH");
+            MapP6();
+        },
+             MapP6: function () {
+        console.log("P6");
+        let directions = ['North', 'West', 'East', 'South'],
+            index = readlineSync.keyInSelect(directions, 'What do you do now??');
+
+        if (directions[index] == "North") {
+            console.log("MOVED NORTH!!!");
+            MapP5();
+        }
+
+        if (directions[index] == "West") {
+            console.log("MOVED WEST!!");
+            MapO6();
+        }
+
+        if (directions[index] == "East") {
+            console.log("CANT MOVE HERE!!");
+            MapP5();
+        }
+
+        if (directions[index] == "South") {
+            console.log("CANT MOVE HERE");
+            MapP6();
+        },
+             MapP8: function () {
+        console.log("P8");
+        let directions = ['North', 'West', 'East', 'South'],
+            index = readlineSync.keyInSelect(directions, 'What do you do now??');
+
+        if (directions[index] == "North") {
+            console.log("Cant Move Here!!!");
+            MapP8();
+        }
+
+        if (directions[index] == "West") {
+            console.log("MOVED WEST!!");
+            MapO8();
+        }
+
+        if (directions[index] == "East") {
+            console.log("CANT MOVE HERE!!");
+            MapP8();
+        }
+
+        if (directions[index] == "South") {
+            console.log("CANT MOVE HERE");
+            MapP8();
+            
     }
 };
